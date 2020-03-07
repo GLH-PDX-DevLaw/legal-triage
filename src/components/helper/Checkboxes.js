f131577bff6bc5a63c2587cd595c2b2c5b10d190
Eve Stockstill <evestockstillpdx@gmail.com>
2020-03-07 13:36:10 -0800

    checkbox

---
+++ src/components/helper/Checkboxes.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Checkbox from './Checkbox';
+
+const Checkboxes = ({ checkboxes, name, onChange, checked }) => {
+  const checkboxElements = checkboxes.map(({ label, value }) => (
+    <Checkbox
+      key={value}
+      label={label}
+      value={value}
+      name={name}
+      checked={checked}
+      onChange={onChange}
+    />
+  ));
+
+  return <>{checkboxElements}</>;
+};
+
+Checkboxes.propTypes = {
+  checkboxes: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.bool.isRequired
+};
+
+export default RadioButtons;
