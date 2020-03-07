32e5d8e7204076547c25ea4f0f80e5d799ed1f0c
Dirt Deodara <thecrowleaps@gmail.com>
2020-03-07 12:19:55 -0800

    brought Home into App

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
 import Header from './header/Header';
-import Home from './Home/Home.js';
+import Home from './home/Home.js';
 import Questionnaire from './questionnaire/Questionnaire.js';
 import Results from './results/Results.js';
 
 export default function App() {
+  const [questionnaire, setQuestionnaire] = useState({});
+
   return (
     <>
       <Router>
         <Header />
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' render={() => {
+            return (
+              <Home setQuestionnaire={setQuestionnaire} />
+            )
+          }} />
           <Route path='/questionnaire' component={Questionnaire} />
           <Route path='/results' component={Results} />
         </Switch>
---
+++ src/components/home/Home.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import PropTypes from 'prop-types';
+
+
+const Home = ({ setQuestionnaire }) => {
+
+  const [isOpen, setIsOpen] = useState(true)
+
+  Modal.setAppElement('#root')
+
+  const openModal = () => {
+    setIsOpen(true);
+  }
+ 
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+  }
+ 
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
+  
+  
+  return (
+    <>
+        <Modal
+          isOpen={isOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          // style={customStyles}
+          contentLabel="Example Modal"
+        >
+ 
+          {/* <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2> */}
+          <button onClick={closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+        <p>hola mundo</p>
+      {/* <Button setQuestionnaire={setQuestionnaire}/>
+      <Button setQuestionnaire={setQuestionnaire}/>
+      <Button setQuestionnaire={setQuestionnaire}/>
+      <Button setQuestionnaire={setQuestionnaire}/> */}
+    </>
+  )
+}
+
+Home.propTypes = {
+  setQuestionnaire: PropTypes.func.isRequired
+}
+
+export default Home;
