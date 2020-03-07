093e7f11cc26c22e60ccb5f7fe90361980e25c06
Ian Andrewson <ianandrewson@gmail.com>
2020-03-07 11:51:24 -0800

    Router and Switch set up. Skeletons made and implelemented for Header, Questionnaire, and Results

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Header from './header/Header';
+import Home from './Home/Home.js';
+import Questionnaire from './questionnaire/Questionnaire.js';
+import Results from './results/Results.js';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/questionnaire' component={Questionnaire} />
+          <Route path='/results' component={Results} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
-  
