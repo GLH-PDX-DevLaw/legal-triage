e33b73935104a2aa270f394c20ce707f0303f9fc
Dirt Deodara <thecrowleaps@gmail.com>
2020-03-07 15:35:10 -0800

    brought in context to App

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Header from './header/Header';
 import Home from './home/Home.js';
 import Questionnaire from './questionnaire/Questionnaire.js';
 import Results from './results/Results.js';
+import { LawAreaProvider } from '../hooks/context';
 
 export default function App() {
 
@@ -15,11 +16,13 @@ export default function App() {
     <>
       <Router>
         <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/questionnaire/:area' component={Questionnaire} />
-          <Route path='/results' component={Results} />
-        </Switch>
+        <LawAreaProvider>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/questionnaire/:area' component={Questionnaire} />
+            <Route path='/results' component={Results} />
+          </Switch>
+        </LawAreaProvider>
       </Router>
     </>
   );
