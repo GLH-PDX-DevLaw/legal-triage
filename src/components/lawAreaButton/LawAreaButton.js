1e78ff24447ea142d013ab2dc44b1c2e3ee55e5c
Dirt Deodara <thecrowleaps@gmail.com>
2020-03-07 16:56:28 -0800

    changed what hook we are using to useSetQuestionnaire

--- src/components/lawAreaButton/LawAreaButton.js
+++ src/components/lawAreaButton/LawAreaButton.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import styles from './LawAreaButton.css';
-import useLawArea from '../../hooks/useLawArea';
-
-
 import { useHistory } from 'react-router-dom';
 import { useSetQuestionnaire } from '../../hooks/context';
 
-
 const LawAreaButton = ({ area }) => {
   const history = useHistory();
-  const { setQuestionnaire } = useSetQuestionnaire()
+  const { setQuestionnaire } = useSetQuestionnaire();
   return (
-    <>
-
-      <div className={styles.lawButtonContainer}>
-        <button
-          className={styles.mainLawButton}
-          onClick={() => {
-        setQuestionnaire(area.label)
-        history.push(`/questionnaire/${area.areaParam}`);
-      }}
-        >
-          <img src={area.icon} className={styles.lawImage} />
-          <p className={styles.lawArea}>{area.label}</p>
-        </button>
-      </div>
-
-    </>
+    <div className={styles.lawButtonContainer}>
+      <button
+        className={styles.mainLawButton}
+        onClick={() => {
+          setQuestionnaire(area);
+          history.push(`/questionnaire/${area.areaParam}`);
+        }}
+      >
+        <img src={area.icon} className={styles.lawImage} />
+        <p className={styles.lawArea}>{area.label}</p>
+      </button>
+    </div>
   );
 };
 
